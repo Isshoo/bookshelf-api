3889ea6fbf3a50ecf41062155dd3613255a8fd91
Tods <algy25ng@gmail.com>
2024-10-15 16:56:41 +0800

    edit book without name and pageRead>pageCount

--- src/handler.js
+++ src/handler.js
@@ -59,7 +59,7 @@ const addBookHandler = (request, h) => {
 const getAllBooksHandler = () => ({
   status: 'success',
   data: {
-    books
+    books: books,
   },
 });
 
@@ -90,6 +90,24 @@ const editBookByIdHandler = (request, h) => {
 
   const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
   const updatedAt = new Date().toISOString();
+
+  if (!name){
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (readPage > pageCount){
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
 
   const index = books.findIndex((book) => book.id === id);
 
